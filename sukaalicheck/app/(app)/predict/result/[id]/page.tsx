3e829b683221d98b79e4226346818f8bc4269ded
lodkazibwe <lodkazibwe@gmail.com>
2026-06-05 16:49:51 +0300

    blinking page fix

--- sukaalicheck/app/(app)/predict/result/[id]/page.tsx
+++ sukaalicheck/app/(app)/predict/result/[id]/page.tsx
@@ -1,15 +1,9 @@
-import { MOCK_RECORDS } from "@/lib/mock";
 import { ResultClient } from "./result-client";
 
 export function generateStaticParams() {
-  return MOCK_RECORDS.map((r) => ({ id: r.id }));
+  return [{ id: "_" }];
 }
 
-export default async function ResultPage({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const { id } = await params;
-  return <ResultClient id={id} />;
+export default async function ResultPage() {
+  return <ResultClient />;
 }
